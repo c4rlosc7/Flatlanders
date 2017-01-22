f506be67a3e4ec6005459797e92b2f4f0f77b0b8
c4rlosc7 <c4rlosc7@gmail.com>
2017-01-22 11:53:55 -0500

    use the images

--- js/app.js
+++ js/app.js
@@ -9,13 +9,23 @@
 			name: 'Dodecahedron',
 			price: 2.95,
 			description: 'Some gems have hidden qualites beyond their luster, beyond their shine...',
-			canPurchase: true
+			canPurchase: true,
+			images: [
+		        "images/gem-01.gif",
+		        "images/gem-03.gif",
+		        "images/gem-04.gif"
+      		]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
-			description: 'Some gems have hidden qualites beyond their luster, beyond their shine...',
-			canPurchase: false
+			description: '.................................................................................',
+			canPurchase: false,
+		    images: [
+		        "images/gem-06.gif",
+		        "images/gem-07.gif",
+		        "images/gem-10.gif"
+		    ]			
 		}
 	]
 })();
